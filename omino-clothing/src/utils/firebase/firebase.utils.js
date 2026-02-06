1055726723ca3473fba0aef67b5e7c67535d3d60
emmanuel <esongz456@gmail.com>
2026-02-06 17:56:07 +0100

    created authentication page component

--- omino-clothing/src/utils/firebase/firebase.utils.js
+++ omino-clothing/src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 import { getAnalytics, logEvent } from "firebase/analytics";
 
@@ -58,6 +58,12 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
     if(!email || !password) return;
 
     return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
 }
 
 const analytics = getAnalytics(firebaseApp);
