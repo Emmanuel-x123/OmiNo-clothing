dd8f3ba87f4d99d80aaf9cbbd5f61dfafe366461
emmanuel <esongz456@gmail.com>
2026-02-06 09:45:19 +0100

    added sign component

---
+++ omino-clothing/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link } from "react-router-dom";
+// import {ReactComponent as Ominologo} from '../../assets/omino-logo2.svg';
+import logo from '../../assets/omino-logo.jpeg';
+import './navigation.styles.scss';
+
+
+const Navigation = () => {
+    return (
+        <>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <img src={logo} alt="omino Logo" className="logo" />
+                     {/* <Ominologo className="logo" /> */}
+                </Link>  
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>SHOP</Link>
+                    <Link className="nav-link" to='/contact'>CONTACT</Link>
+                    <Link className="nav-link" to='/signIn'>SIGN IN</Link>
+                </div>
+            </div>
+            <Outlet />
+        </>
+    )
+}
+
+export default Navigation;
---
+++ omino-clothing/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,56 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getAnalytics, logEvent } from "firebase/analytics";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADMksA1qJoXo8OVaf0Jy9viTcu5mbg4vc",
+  authDomain: "omino-clothing-db.firebaseapp.com",
+  projectId: "omino-clothing-db",
+  storageBucket: "omino-clothing-db.firebasestorage.app",
+  messagingSenderId: "680351925053",
+  appId: "1:680351925053:web:32ef5f3546349b44cffb54",
+  measurementId: "G-EYYN1DNLYN"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: 'select_account'
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        }catch(error){
+            console.log('error creating the user', error.message);
+    }
+}
+    return userDocRef;
+
+}
+
+const analytics = getAnalytics(firebaseApp);
+
+logEvent(analytics, 'notification_received');
+
